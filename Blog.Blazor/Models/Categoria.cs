39c7268122136b3ae0b0d6f5b226ec971a994f41
Daniel Pelissari <dpelissari1@gmail.com>
2024-04-15 22:28:58 -0300

    Página de gerenciamento das categorias dos posts
    Inclusão e edição de categorias

--- Blog.Blazor/Models/Categoria.cs
+++ Blog.Blazor/Models/Categoria.cs
@@ -10,16 +10,16 @@ namespace Blog.Blazor.Models
         
         [MaxLength(250)]
         [Required(ErrorMessage = "O campo nome é obrigatório")]
-        public string Nome { get; set; }
+        public string? Nome { get; set; }
 
         [Required(ErrorMessage = "O campo descrição é obrigatório")]
         [MaxLength(500, ErrorMessage = "A descrição deve ter no máximo 500 caracteres")]
-        public string Descricao { get; set; }
+        public string? Descricao { get; set; }
 
         [Required(ErrorMessage = "O campo data de cadastro é obrigatório")]
         public DateTime Cadastro { get; set; } = DateTime.Now;
 
-        public ICollection<Post> Posts { get; set; }
+        public ICollection<Post>? Posts { get; set; }
 
         [Required(ErrorMessage = "O campo ativo é obrigatório")]
         public bool Ativo { get; set; } = true;
