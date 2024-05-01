9c23dcfcb4acd305aa2370cadd06801ac89f5c02
Daniel Pelissari <dpelissari1@gmail.com>
2024-04-30 23:14:14 -0300

    Servico para controle de sessao do usuario

--- Blog.Blazor/Program.cs
+++ Blog.Blazor/Program.cs
@@ -22,6 +22,7 @@ builder.Services.AddScoped<IPostService, PostService>();
 builder.Services.AddScoped<IAutorService, AutorService>();
 builder.Services.AddScoped<ICategoriaService, CategoriaService>();
 builder.Services.AddScoped<IUsuarioService, UsuarioService>();
+builder.Services.AddSingleton<UsuarioAutenticadoService>();
 builder.Services.AddControllers();
 
 var app = builder.Build();
---
+++ Blog.Blazor/Services/UsuarioAutenticadoService.cs
@@ -0,0 +1,21 @@
+﻿using Blog.Blazor.Models;
+
+namespace Blog.Blazor.Services
+{
+    public class UsuarioAutenticadoService
+    {
+        private Usuario _currentUser;
+
+        public Usuario CurrentUser => _currentUser;
+
+        public bool IsUserLoggedIn => _currentUser != null;
+
+        public event Action OnUserChanged;
+
+        public void SetCurrentUser(Usuario user)
+        {
+            _currentUser = user;
+            OnUserChanged?.Invoke();
+        }
+    }
+}
