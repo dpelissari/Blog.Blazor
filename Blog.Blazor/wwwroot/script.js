e1e43908c350f6a2f59c21843cdff0d5ac67461d
Daniel Pelissari <dpelissari1@gmail.com>
2024-05-07 17:10:56 -0300

    Add Swiper na pagina inicial

--- Blog.Blazor/wwwroot/script.js
+++ Blog.Blazor/wwwroot/script.js
@@ -11,3 +11,21 @@ window.closeMenu = function () {
     navbarBurger.classList.remove('is-active');
     navbarMenu.classList.remove('is-active');
 };
+
+
+window.swiperFunctions = {
+    initializeSwiper: function (elementId) {
+        var swiper = new Swiper("." + elementId, {
+
+            pagination: {
+                el: ".swiper-pagination",
+                clickable: true,
+            },
+            breakpoints: {
+                640: { slidesPerView: 1, spaceBetween: 20, },
+                768: { slidesPerView: 2, spaceBetween: 40, },
+                1024: { slidesPerView: 4, spaceBetween: 30, },
+            },
+        });
+    }
+};
