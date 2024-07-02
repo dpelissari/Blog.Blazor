59bfe7084b3ec3420a1bc97b261315a66b3f731c
Daniel Pelissari <dpelissari1@gmail.com>
2024-07-02 16:47:17 -0300

    Busca paginada

--- Blog.Blazor/Interfaces/IPostService.cs
+++ Blog.Blazor/Interfaces/IPostService.cs
@@ -5,22 +5,19 @@ namespace Blog.Blazor.Interfaces
 {
     public interface IPostService
     {
-        // assinatura dos metodos a serem implementados pela classe PostService
         Task Adicionar(Post Post);
         Task Atualizar(Post Post);
         Task Apagar(Post Post);
-        Task<Post> BuscarPor(Guid id);
-        Task<IEnumerable<Post>> BuscarPorAutorId(Guid id);
-        Task<IEnumerable<Post>> BuscarPorIdCategoria(Guid id);
-        Task<IEnumerable<Post>> BuscarTodos();
-        Task<IEnumerable<Post>> BuscarPostsPaginados(int paginaAtual, int itensPorPagina);
+        Task<IEnumerable<Post>> BuscarTodosPosts();
+        Task<Post> BuscarPorIdPost(Guid id);
+        Task<IEnumerable<Post>> BuscarPostsPorIdAutor(Guid id);
+        Task<IEnumerable<Post>> BuscarPostsPorIdCategoria(Guid id);
         Task<Dictionary<string, List<Post>>> BuscarPostsAgrupadosPorCategoria(int paginaAtual, int categoriasPorPagina);
-        Task<int> ObterTotalDeCategorias();
-
-        Task<IEnumerable<Post>> BuscarPorTexto(string texto);
-
-        Task<IEnumerable<Post>> BuscarPorTextoPaginado(string texto, int paginaAtual, int itensPorPagina);
-
+        Task<IEnumerable<Post>> BuscarPostsPaginados(int paginaAtual, int itensPorPagina);
+        Task<IEnumerable<Post>> BuscarPostsPorTexto(string texto);
+        Task<IEnumerable<Post>> BuscarPostsPorTextoPaginado(string texto, int paginaAtual, int itensPorPagina);
         Task<int> ObterTotalDePosts();
+        Task<int> ObterTotalDeCategorias();
+        Task<int> ObterTotalDePostsPorTexto(string texto);
     }
 }
--- Blog.Blazor/Services/PostService.cs
+++ Blog.Blazor/Services/PostService.cs
@@ -16,14 +16,20 @@ namespace Blog.Blazor.Services
             dbContexto = appDbContexto;
         }
 
-        // metodo asincrono para adicionar post
+        /// <summary>
+        /// Metodo para adicionar uma publicação/post
+        /// </summary>
+        /// <param name="post"></param>
         public async Task Adicionar(Post post)
         {
             await dbContexto.AddAsync(post);
             await dbContexto.SaveChangesAsync();
         }
 
-        // metodo asincrono para atualizar post
+        /// <summary>
+        /// Metodo para atualizar uma publicação/post
+        /// </summary>
+        /// <param name="post"></param>
         public async Task Atualizar(Post post)
         {
             post.UltimaAtualizacao = DateTime.Now;
@@ -31,36 +37,57 @@ namespace Blog.Blazor.Services
             await dbContexto.SaveChangesAsync();
         }
 
-        // metodo asincrono para apagar posts
+        /// <summary>
+        /// Metodo para excluir uma publicação/post
+        /// </summary>
+        /// <param name="post"></param>
         public async Task Apagar(Post post)
         {
             dbContexto.Remove(post);
             await dbContexto.SaveChangesAsync();
         }
 
-        // metodo para retornar um post com base no id
-        public async Task<Post> BuscarPor(Guid id)
-        {
-            return await dbContexto.Post.FirstOrDefaultAsync(f => f.Id == id);
-        }
-
-        // metodo para listar todos posts
-        public async Task<IEnumerable<Post>> BuscarTodos()
+        /// <summary>
+        /// Obtem todas publicações/posts sem nenhum filtro
+        /// </summary>
+        public async Task<IEnumerable<Post>> BuscarTodosPosts()
         {
             return await dbContexto.Post.ToListAsync();
         }
 
-        // metodo para listar todos posts de um autor
-        public async Task<IEnumerable<Post>> BuscarPorAutorId(Guid autorId)
+        /// <summary>
+        /// Obtem uma publicação/post pelo id
+        /// </summary>
+        /// <param name="id"></param>
+        public async Task<Post> BuscarPorIdPost(Guid id)
+        {
+            return await dbContexto.Post.FirstOrDefaultAsync(f => f.Id == id);
+        }
+
+        /// <summary>
+        /// Obtem todas publicações/posts de um autor
+        /// </summary>
+        /// <param name="autorId"></param>
+        public async Task<IEnumerable<Post>> BuscarPostsPorIdAutor(Guid autorId)
         {
             return await dbContexto.Post.Where(f => f.Id == autorId).ToListAsync();
         }
 
-        public async Task<IEnumerable<Post>> BuscarPorIdCategoria(Guid categoriaId)
+        /// <summary>
+        /// Obtem todas publicações/posts de uma categoria
+        /// </summary>
+        /// <param name="categoriaId"></param>
+        public async Task<IEnumerable<Post>> BuscarPostsPorIdCategoria(Guid categoriaId)
         {
             return await dbContexto.Post.Where(f => f.IdCategoria == categoriaId).ToListAsync();
         }
 
+
+        /// <summary>
+        /// Obtem publicações/posts agrupadas por categoria
+        /// </summary>
+        /// <param name="paginaAtual"></param>
+        /// <param name="categoriasPorPagina"></param>
         public async Task<Dictionary<string, List<Post>>> BuscarPostsAgrupadosPorCategoria(int paginaAtual, int categoriasPorPagina)
         {
             var todosPosts = await dbContexto.Post.Include(post => post.Categoria).OrderBy(post => post.Categoria.Nome).ToListAsync();
@@ -68,12 +95,56 @@ namespace Blog.Blazor.Services
             return categoriasPaginadas;
         }
 
-
+        /// <summary>
+        /// Obtem publicações/posts de forma paginada
+        /// </summary>
+        /// <param name="paginaAtual"></param>
+        /// <param name="itensPorPagina"></param>
+        /// <returns></returns>
         public async Task<IEnumerable<Post>> BuscarPostsPaginados(int paginaAtual, int itensPorPagina)
         {
             return await dbContexto.Post.Skip((paginaAtual - 1) * itensPorPagina).Take(itensPorPagina).ToListAsync();
         }
 
+        /// <summary>
+        /// Metodo para buscar publicações/posts que contenham um termo especificado
+        /// </summary>
+        /// <param name="texto"></param>
+        public async Task<IEnumerable<Post>> BuscarPostsPorTexto(string texto)
+        {
+            return await dbContexto.Post
+                .Where(p => EF.Functions.Like(p.Titulo, $"%{texto}%") || EF.Functions.Like(p.Conteudo, $"%{texto}%") || EF.Functions.Like(p.Categoria.Nome, $"%{texto}%"))
+                .ToListAsync();
+        }
+
+        /// <summary>
+        /// Metodo para buscar publicações/posts que contenham um termo especificado de forma paginada
+        /// </summary>
+        /// <param name="texto"></param>
+        /// <param name="paginaAtual"></param>
+        /// <param name="itensPorPagina"></param>
+        public async Task<IEnumerable<Post>> BuscarPostsPorTextoPaginado(string texto, int paginaAtual, int itensPorPagina)
+        {
+            return await dbContexto.Post
+                .Where(p => EF.Functions.Like(p.Titulo, $"%{texto}%") || EF.Functions.Like(p.Conteudo, $"%{texto}%") || EF.Functions.Like(p.Categoria.Nome, $"%{texto}%"))
+                .Skip((paginaAtual - 1) * itensPorPagina)
+                .Take(itensPorPagina)
+                .ToListAsync();
+        }
+
+        /// <summary>
+        /// Retorna o numero total de publicações/posts
+        /// </summary>
+        /// <returns></returns>
+        public async Task<int> ObterTotalDePosts()
+        {
+            return await dbContexto.Post.CountAsync();
+        }
+
+        /// <summary>
+        /// Retorna o total de categorias de publicações/posts
+        /// </summary>
+        /// <returns></returns>
         public async Task<int> ObterTotalDeCategorias()
         {
             var allPosts = await dbContexto.Post.Include(post => post.Categoria).ToListAsync();
@@ -81,27 +152,16 @@ namespace Blog.Blazor.Services
             return groupedPosts;
         }
 
-        // Método de busca por texto
-        public async Task<IEnumerable<Post>> BuscarPorTexto(string texto)
+        /// <summary>
+        /// Retorna o total de posts correspondentes a um termo especificado
+        /// </summary>
+        /// <param name="texto"></param>
+        /// <returns></returns>
+        public async Task<int> ObterTotalDePostsPorTexto(string texto)
         {
             return await dbContexto.Post
                 .Where(p => EF.Functions.Like(p.Titulo, $"%{texto}%") || EF.Functions.Like(p.Conteudo, $"%{texto}%"))
-                .ToListAsync();
+                .CountAsync();
         }
-
-        public async Task<IEnumerable<Post>> BuscarPorTextoPaginado(string texto, int paginaAtual, int itensPorPagina)
-        {
-            return await dbContexto.Post
-                .Where(p => EF.Functions.Like(p.Titulo, $"%{texto}%") || EF.Functions.Like(p.Conteudo, $"%{texto}%"))
-                .Skip((paginaAtual - 1) * itensPorPagina)
-                .Take(itensPorPagina)
-                .ToListAsync();
-        }
-
-        public async Task<int> ObterTotalDePosts()
-        {
-            return await dbContexto.Post.CountAsync();
-        }
-
     }
 }
